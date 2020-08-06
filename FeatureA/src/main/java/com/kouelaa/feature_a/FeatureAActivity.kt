ce230a0e1e22ad37d243e0121d138ed2797ff754
Kheireddine Ouelaa <kheireddine.ouelaa@capgemini.com>
2020-08-06 18:08:24 +0200

    Adding progression of downloading feature

--- FeatureA/src/main/java/com/kouelaa/feature_a/FeatureAActivity.kt
+++ FeatureA/src/main/java/com/kouelaa/feature_a/FeatureAActivity.kt
@@ -1,22 +1,119 @@
 package com.kouelaa.feature_a
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.google.android.play.core.splitcompat.SplitCompat
+import com.google.android.play.core.splitinstall.*
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
+import com.google.android.play.core.splitinstall.testing.FakeSplitInstallManager
+import com.google.android.play.core.splitinstall.testing.FakeSplitInstallManagerFactory
 import kotlinx.android.synthetic.main.activity_feature_a.*
+import java.lang.Exception
 
-class FeatureAActivity : AppCompatActivity() {
+class FeatureAActivity : AppCompatActivity(),
+    SplitInstallStateUpdatedListener {
+
+    private lateinit var manager: SplitInstallManager
+    private var mySessionId = 0
+
+
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        // Emulates installation of on demand modules using SplitCompat.
+        SplitCompat.installActivity(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feature_a)
 
+
+
+        // TODO-(05/08/20)-kheirus:  We can later use DI to inject this
+        manager = SplitInstallManagerFactory.create(this)
+
+        // TODO-(05/08/20)-kheirus:  Here we have to test if the module is already downloaded or not before calling it, otherwise the app crashes
+        // TODO-(05/08/20)-kheirus:  find solution to request the name of the feature from an asset and not hard code it
+
         go_feature_b.setOnClickListener {
-            startActivity(
-                Intent().setClassName(
-                    "com.kouelaa.pocdynamicfeatures",
-                    "com.kouelaa.feature_b.FeatureBActivity"
-                )
+            val request = SplitInstallRequest.newBuilder()
+                .addModule("FeatureB")
+                .build()
+
+            manager.registerListener (this )
+
+            manager.startInstall(request)
+                .addOnSuccessListener{ sessionId -> mySessionId = sessionId }
+                .addOnFailureListener{ exception -> failureInstallingFeatureB(exception) }
+
+        }
+        manager.unregisterListener(this)
+    }
+
+
+    private fun failureInstallingFeatureB(e: Exception) {
+        Toast.makeText(this, "Error loading Feature B from Play Store: $e", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    private fun showProgress(progressPercent: Long) {
+        go_feature_b.isEnabled = false
+        progress.visibility = View.VISIBLE
+        progress_tv.visibility = View.VISIBLE
+
+        progress.progress = progressPercent.toInt()
+    }
+
+    private fun startFeatureB() {
+        progress.visibility = View.GONE
+        progress_tv.visibility = View.GONE
+        go_feature_b.isEnabled = true
+
+        startActivity(
+            Intent().setClassName(
+                "com.kouelaa.pocdynamicfeatures",
+                "com.kouelaa.feature_b.FeatureBActivity"
             )
+        )
+    }
+
+    override fun onStateUpdate(state : SplitInstallSessionState) {
+        if (state.status() == SplitInstallSessionStatus.FAILED)
+            {
+            // // TODO-(06/08/20)-kheirus: Make a dialog or anything else to Retry the request.
+            return
+        }
+        if (state.sessionId() == mySessionId) {
+            when (state.status()) {
+                SplitInstallSessionStatus.DOWNLOADING -> {
+                    val totalBytes = state.totalBytesToDownload()
+                    val progress = state.bytesDownloaded()
+                    showProgress(progress*100/totalBytes)
+                }
+                SplitInstallSessionStatus.INSTALLED -> {
+
+                    // After a module is installed, you can start accessing its content or
+                    // fire an intent to start an activity in the installed module.
+                    // For other use cases, see access code and resources from installed modules.
+
+                    // If the request is an on demand module for an Android Instant App
+                    // running on Android 8.0 (API level 26) or higher, you need to
+                    // update the app context using the SplitInstallHelper API.
+
+                    startFeatureB()
+                }
+
+                else -> Unit
+            }
         }
     }
+
 }
--- FeatureB/src/main/java/com/kouelaa/feature_b/FeatureBActivity.kt
+++ FeatureB/src/main/java/com/kouelaa/feature_b/FeatureBActivity.kt
@@ -1,10 +1,19 @@
 package com.kouelaa.feature_b
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.play.core.splitcompat.SplitCompat
 import kotlinx.android.synthetic.main.activity_feature_b.*
 
 class FeatureBActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        // Emulates installation of on demand modules using SplitCompat.
+        SplitCompat.installActivity(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feature_b)
